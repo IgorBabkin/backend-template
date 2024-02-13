93ec760fa7f734ee9406e829a08356798b3e3b5c
ibabkin <babkin.i@obelis.ltd>
2024-02-13 19:26:52 +0000

    refactor: cleanup

--- src/lib/zod/utils.ts
+++ src/lib/zod/utils.ts
@@ -1,8 +1,3 @@
-import { z, ZodType } from 'zod';
+import { z } from 'zod';
 
 export const zNumber = z.string().regex(/^\d+$/).transform(Number);
-
-export const validate =
-  <T extends ZodType>(validator: T) =>
-  (value: unknown) =>
-    validator.parse(value);
