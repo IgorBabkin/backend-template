c6d2c893cd1a7a9c5775acd1b48e09a76b89b47c
ibabkin <babkin.i@obelis.ltd>
2024-04-25 22:01:32 +0200

    refactor(todo): cleanup code

--- src/useCase/todo/FindTodoMiddleware.ts
+++ src/useCase/todo/FindTodoMiddleware.ts
@@ -4,7 +4,6 @@ import { ITodoRepo, ITodoRepoKey } from '../../domains/todo/TodoRepo';
 import { perScope } from '../../lib/mediator/Scope';
 import { asMiddleware } from '../../lib/container/Middleware';
 import { FillQueryMiddleware } from '../middleware/FillQueryMiddleware';
-import * as console from 'node:console';
 import { ITodoQuery, WithTodo } from '../middleware/IAppQuery';
 
 @register(scope(perScope.Request))
@@ -14,12 +13,11 @@ export class FindTodoMiddleware extends FillQueryMiddleware<ITodoQuery> {
     super();
   }
 
-  protected async execute(query: ITodoQuery): Promise<void> {
-    (query as WithTodo<ITodoQuery>).todo = await this.em.findByIdOrFail(query.todoID);
+  protected matchQuery(query: unknown): query is ITodoQuery {
+    return 'todoID' in (query as any);
   }
 
-  protected matchQuery(query: unknown): query is ITodoQuery {
-    console.log('FindTodo middleware');
-    return 'todoID' in (query as any);
+  protected async execute(query: ITodoQuery): Promise<void> {
+    (query as WithTodo<ITodoQuery>).todo = await this.em.findByIdOrFail(query.todoID);
   }
 }
